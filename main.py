52f9a01fe3878e3100bb44bc2c88e4c5f0aa2eb5
Michael <mail@heidemichael.de>
2020-06-09 10:53:36 +0200

    save interval

--- main.py
+++ main.py
@@ -2,15 +2,8 @@ import torch
 from torch import nn , optim
 import torch.nn.functional as F
 
-
 import numpy as np
 import matplotlib.pyplot as plt
-import PIL
-from time import time
-from matplotlib import pyplot
-
-from PIL import Image
-import torchvision
 from torchvision import datasets, transforms
 
 #Settings
@@ -18,14 +11,13 @@ inputSize = 784
 hiddenLayerSize = 100
 outputSize = 5
 
-epochs = 200
+epochs = 100
 #LearningRate AdamOptimizer
-learning_rate = 0.0002
-l2_reg = 0.00003
-
+learning_rate = 0.01
+l2_reg = 0.001
 
 class Net(nn.Module):
-    gainFactor = .7
+    gainFactor = 1
 
     def __init__(self):
         super(Net, self).__init__()
@@ -62,27 +54,24 @@ class Net(nn.Module):
         self.fc7 = nn.Linear(hiddenLayerSize, outputSize)
         nn.init.xavier_normal_(self.fc7.weight, self.gainFactor)
 
-
-
-
     def forward(self, x):
         x = self.fc1(x)
-        x = F.relu(x)
+        x = F.relu(self.bn1(x))
         x = F.dropout(x)
         x = self.fc2(x)
-        x = F.relu(x)
+        x = F.relu(self.bn2(x))
         x = F.dropout(x)
         x = self.fc3(x)
-        x = F.relu(x)
+        x = F.relu(self.bn3(x))
         x = F.dropout(x)
         x = self.fc4(x)
-        x = F.relu(x)
+        x = F.relu(self.bn4(x))
         x = F.dropout(x)
         x = self.fc5(x)
-        x = F.relu(x)
+        x = F.relu(self.bn5(x))
         x = F.dropout(x)
         x = self.fc6(x)
-        x = F.relu(x)
+        x = F.relu(self.bn6(x))
         x = F.dropout(x)
         x = self.fc7(x)
         x = F.relu(x)
@@ -92,25 +81,33 @@ class Net(nn.Module):
 
         return output
 
-
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
-
-def getAccuracy(model, data):
+def getAccuracy(model, data, val=False):
     correct = 0
     total = 0
-    model.eval() #*********#
+    losses = []
+    model.eval()
     for imgs, labels in data:
         imgs = imgs.view(imgs.size(0), -1)
 
         output = model(imgs)
-        pred = output.max(1, keepdim=True)[1] # get the index of the max logit
-        correct += pred.eq(labels.view_as(pred)).sum().item()
+        _, predicted = torch.max(output.data, 1)
+        if val:
+            loss = loss_func(output, labels)
+            losses.append(loss.data.cpu().numpy())
+
+        correct += (predicted == labels).sum().item()
         total += imgs.shape[0]
+
+
+    if val:
+        losses_val.append(np.mean(losses).item())
+    print('Accurarcy: {}'.format(correct / total))
     return correct / total
 
 # Define a transform to normalize the data
@@ -127,23 +124,14 @@ idx = (trainset.targets < 5)
 trainset.targets = trainset.targets[idx]
 trainset.data = trainset.data[idx]
 
-trainloader = torch.utils.data.DataLoader(trainset, shuffle=False, batch_size=128)
+trainloader = torch.utils.data.DataLoader(trainset, shuffle=True, batch_size=512)
 
 idx = (valset.targets < 5)
 
 valset.targets = valset.targets[idx]
 valset.data = valset.data[idx]
 
-valloader = torch.utils.data.DataLoader(valset, shuffle=False, batch_size=128)
-
-
-classes = ('0', '1', '2', '3',
-           '4', '5', '6', '7', '8', '9')
-
-
-
-
-
+valloader = torch.utils.data.DataLoader(valset, shuffle=False, batch_size=512)
 
 ## Creating Network Module
 
@@ -153,7 +141,7 @@ net = Net()
 adamOptimizer = optim.Adam(net.parameters(), lr=learning_rate,  weight_decay=l2_reg)
 loss_func = nn.CrossEntropyLoss()
 
-iters, losses_all, train_acc, val_acc = [], [], [], []
+iters, losses_all, losses_val, train_acc, val_acc =[], [], [], [], []
 
 for epoch in range(epochs):
     losses = []
@@ -180,14 +168,20 @@ for epoch in range(epochs):
                 epoch, index+1, np.mean(losses).item()))
 
             running_loss = 0.0
+
+    if epoch % 20 == 0:
+        PATH = './trained_network_epoch_{}.pth'.format(epoch)
+        torch.save(net.state_dict(), PATH)
+
+
     iters.append(epoch)
     losses_all.append(np.mean(losses).item())  # compute *average* loss
     train_acc.append(getAccuracy(net, trainloader))  # compute training accuracy
-    val_acc.append(getAccuracy(net, valloader))  # compute validation accuracy
+    val_acc.append(getAccuracy(net, valloader, val=True))  # compute validation accuracy
 
 
 
-PATH = './trained_network.pth'
+PATH = './trained_network_final.pth'
 torch.save(net.state_dict(), PATH)
 
 # plotting
@@ -195,6 +189,8 @@ plt.figure(figsize=(10, 4))
 plt.subplot(1, 2, 1)
 plt.title("Training Curve")
 plt.plot(iters, losses_all, label="Train")
+plt.plot(iters, losses_val, label="Val")
+plt.legend(loc='best')
 plt.xlabel("Epoch")
 plt.ylabel("Loss")
 
@@ -210,36 +206,6 @@ plt.show()
 print("Final Training Accuracy: {}".format(train_acc[-1]))
 print("Final Validation Accuracy: {}".format(val_acc[-1]))
 
-#dataiter = iter(valloader)
-#images, labels = dataiter.next()
-
-
-#imshow(torchvision.utils.make_grid(images))
-#print('GroundTruth: ', ' '.join('%5s' % labels))
-#
-#net = Net()
-#net.load_state_dict(torch.load(PATH))
-#
-#outputs = net(images.view(images.size(0), -1))
-#
-#_, predicted = torch.max(outputs, 1)
-#
-#print('Predicted: ', ' '.join('%5s' % predicted))
-#correct = 0
-#total = 0
-#with torch.no_grad():
-#    for data in valloader:
-#        images, labels = data
-#        images = images.view(images.size(0), -1)
-#
-#        outputs = net(images)
-#        _, predicted = torch.max(outputs.data, 1)
-#        total += labels.size(0)
-#        correct += (predicted == labels).sum().item()
-#
-#print('Accuracy of the network on the 10000 test images: %d %%' % (
-#    100 * correct / total))
-
 # Aufgaben
 # AUfgabe 1.1
 # (x) 5 Hidden Layer with 100 neurons
@@ -248,19 +214,28 @@ print("Final Validation Accuracy: {}".format(val_acc[-1]))
 # (x) Adam Optimization
 # (x) L2 regularization
 # (x) softmax output with 5 neurons
-# () save CheckPoints
+# (x) save CheckPoints
 # (x) save final Model
 #
 #Aufgabe 1.2
 #
-# () Tune hyperparameter cross-validation
+# (x) Tune hyperparameter cross-validation
 #
+# Accuracy around 97%
+
 #Aufgabe 1.3
 #
-# () add Batch Normalization
-# () Compare learning curves
+# (x) add Batch Normalization
+# (x) Compare learning curves
+#
+# Model ist nicht besser als vorher. Genauigkeit nimmt ab.
+# Konvergiert mit einem höheren Loss.
+#
 #
 #AUfgabe 1.4
 #
-# () Is Model Overfitting?
-# () add Dropout to every layer
+# (x) Is Model Overfitting?
+# (x) add Dropout to every layer
+#
+#
+
